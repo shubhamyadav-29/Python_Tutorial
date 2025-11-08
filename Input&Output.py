4354effe4113e5ec21b14e2c5cbecb431ed7e70f
anuj maurya <shubhamarjun5@gmail.com>
2025-11-08 14:47:46 +0700

    Initial commit

---
+++ Input&Output.py
@@ -0,0 +1,9 @@
+# Input and Output
+
+name=input("Enter Your Name:")
+
+age=int(input("Enter Your Age:"))
+
+height=float(input("Enter Your Height:"))
+
+print("Name:",name,"Age:",age,"Height:",height)
---
+++ TypeConversion.py
@@ -0,0 +1,9 @@
+#Type Conversion
+x = "10"
+y = int(x)       # converts string to integer
+z = float(x)     # converts string to float
+s = str(100)     # converts integer to string
+
+print(type(y))   # <class 'int'>
+print(type(z))   # <class 'float'>
+print(type(s))   # <class 'str'>
